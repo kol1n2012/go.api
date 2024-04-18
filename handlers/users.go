5e64eb06e580f0a24dec3da8e90260daf047be5a
kol1n2012 <99890520+kol1n2012@users.noreply.github.com>
2024-04-18 10:59:26 +0300

    add file model

--- handlers/users.go
+++ handlers/users.go
@@ -1,24 +1,22 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/kol1n2012/go.api/models"
-	log "github.com/sirupsen/logrus"
+	"github.com/kol1n2012/go.api/models/sourse"
 )
 
 func GetUsers(c *gin.Context) {
 
 	var users = models.Users{}
 
-	setUsersFromFile(&users, "users.json")
+	status, message := sourse.SetCollectionFromFile(&users, "users")
 
 	if err := c.ShouldBindWith(&users, binding.Query); err == nil {
-		c.JSON(http.StatusOK, gin.H{"status": true, "message": "Успешно", "result": users})
+		c.JSON(http.StatusOK, gin.H{"status": status, "message": message, "result": users})
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"status": false, "message": err.Error(), "result": "[]"})
 	}
@@ -31,23 +29,4 @@ func AddUser(c *gin.Context) {
 }
 
 func DeleteUser(c *gin.Context) {
-}
-
-func setUsersFromFile(u *models.Users, file string) {
-
-	pwd, _ := os.Getwd()
-	// Чтение содержимого файла
-	fileData, err := os.ReadFile(pwd + string(os.PathSeparator) + file)
-
-	if err != nil {
-		log.Fatal("ERR_FILE. Ошибка чтения файла: ", err)
-	}
-
-	err = nil
-
-	err = json.Unmarshal([]byte(string(fileData)), &u)
-
-	if err != nil {
-		log.Fatal("ERR_JSON. ошибка распознавания json: ", err)
-	}
 }
